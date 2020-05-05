4ec95f293132210170052689e03c83ab675921b9
Dulaj <balasuriyadulaj@gmail.com>
2020-05-06 01:27:22 +0530

    Update ControlPanel with new button implementation

--- aspire-client/src/components/ControlPanel.jsx
+++ aspire-client/src/components/ControlPanel.jsx
@@ -1,17 +1,49 @@
-import React, { Component } from 'react';
-import ActionButton from './ActionButton';
+import React from 'react';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import { teal, orange } from '@material-ui/core/colors';
 
+const useStyles = makeStyles((theme) => ({
+    style: {
+        margin: theme.spacing(2),
+        paddingLeft: theme.spacing(5),
+        paddingRight: theme.spacing(5),
+        padding: theme.spacing(2)
+    },
+}));
 
-class ControlPanel extends Component {
+const StartButton = withStyles((theme) => ({
+    root: {
+        color: theme.palette.getContrastText(orange[500]),
+        backgroundColor: teal[500],
+        '&:hover': {
+            backgroundColor: teal[700],
+        },
+    },
+}))(Button);
 
-    render() {
+const StopButton = withStyles((theme) => ({
+    root: {
+        color: theme.palette.getContrastText(orange[500]),
+        backgroundColor: orange[500],
+        '&:hover': {
+            backgroundColor: orange[700],
+        },
+    },
+}))(Button);
+
+export default function ControlPanel() {
+    const classes = useStyles();
+
         return (
             <div className="row">
-                <ActionButton kind="primary" value="Start"/>
-                <ActionButton kind="secondary" value="Stop"/>
+                <StartButton variant="contained" className={classes.style}>
+                    Start
+                </StartButton>
+                <StopButton variant="contained" className={classes.style}>
+                    Stop
+                </StopButton>
             </div>
         );
-    }
+    
 }
-
-export default ControlPanel;
